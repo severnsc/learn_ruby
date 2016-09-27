862dca8ec044a71c28a2a71a3b7fa01311c5e00f
Chris Severns <severnsc@gmail.com>
2016-09-27 19:52:28 -0400

    Add lesson 7

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
-  #write your code here
+	attr_accessor :seconds
+
+  def initialize
+  	@seconds = 0
+  	return @seconds
+  end
+
+  def seconds
+  	return @seconds
+  end
+
+  def time_string
+  	time = Time.new(0)
+  	time += seconds
+  	return time.strftime("%H:%M:%S")
+  end
+
 end

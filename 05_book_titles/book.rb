4d3caa95e96c4b19ddeb57b571ac0537a78e7146
Chris Severns <severnsc@gmail.com>
2016-09-27 19:33:28 -0400

    Add lesson 6

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+	 attr_accessor :title
+
+	 def title
+	 	words = @title.split
+	 	conjunctions = ["for", "and", "nor", "but", "or", "yet", "so","in", "at", "on", "the", "of", "a", "an"]
+	 	words[0].capitalize!
+	 	words.each do |x|
+	 		if not conjunctions.include? x
+	 			x.capitalize!
+	 		end
+	 	end
+	 	return words.join(" ")
+	 end
 end
